7eaa118519b2570a520f6bb94cf8d5e9af4015e3
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-05-18 19:04:23 +0300

    learning slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,13 @@ const swiper = new Swiper('.swiper', {
 	slidesPerView: 3,
 	loop: true,
 	spaceBetween: 40,
+	slidesPerGroup: 3,
+	navigation: {
+		prevEl: '.btn__slider--prev',
+		nextEl: '.btn__slider--next',
+	},
+	pagination: {
+        el: ".slider__pag",
+		clickable: true,
+      },
 });
