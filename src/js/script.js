7563da90445f69dcd914615c9b17a3fbabda3f98
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-05-19 03:56:41 +0300

    changes: tabs + fix errors

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,27 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.btn__slider--next',
 	},
 	pagination: {
-        el: ".slider__pag",
+		el: '.slider__pag',
 		clickable: true,
-      },
+	},
+});
+
+const supAll = document.querySelectorAll('.tab__sup');
+const tabAll = document.querySelectorAll('.tab');
+
+supAll.forEach((elem, index) => {
+	elem.addEventListener('click', function () {
+		if (!supAll[index].classList.contains('sup--active')) {
+			supAll.forEach(elem => {
+				elem.classList.remove('sup--active');
+			});
+		}
+		if (!tabAll[index].classList.contains('tab--show')) {
+			tabAll.forEach(elem => {
+				elem.classList.remove('tab--show');
+			});
+		}
+		supAll[index].classList.add('sup--active');
+		tabAll[index].classList.add('tab--show');
+	});
 });
