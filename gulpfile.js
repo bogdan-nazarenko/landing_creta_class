4df040a73870321b526d82cfe8e638fb172f77f0
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-06-25 01:10:07 +0300

    template update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { src, dest, watch, parallel, series } = require('gulp');
 const sass = require('gulp-dart-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const { existsSync } = require('fs');
 (fileinclude = require('gulp-file-include')),
 	(del = require('del')),
 	(htmlmin = require('gulp-htmlmin')),
@@ -13,6 +14,9 @@ const notify = require('gulp-notify');
 	(browserSync = require('browser-sync').create()),
 	(terser = require('gulp-terser')),
 	(browserslist = ['> 1%, last 3 versions, not dead']);
+const plumberHandler = cfg.notify
+	? notify.onError('Error: <%= error.message %>')
+	: function (err) { console.error(err.message); };
 
 function html() {
 	return src([cfg.srcDir + '/*.html'])
@@ -37,22 +41,46 @@ function htmlMin() {
 		.pipe(dest(cfg.outputDir));
 }
 
+// experimental
+// converts otf to ttf, woff, woff2
+// needs gulp-fonter
+// not stable
+
+// async function fontsOTF() {
+// 	const fonter = (await import('gulp-fonter')).default;
+// 	const ttf2woff2 = (await import('gulp-ttf2woff2')).default;
+
+// 	return src(cfg.srcDir + '/fonts/**/*.{ttf,otf}', { encoding: false })
+// 		.pipe(plumber({ errorHandler: plumberHandler }))
+// 		.pipe(fonter({ formats: ['ttf', 'woff'] }))
+// 		.pipe(dest(cfg.outputDir+ '/fonts'))
+// 		.pipe(src(cfg.srcDir + '/fonts/**/*.ttf'))
+// 		.pipe(ttf2woff2())
+// 		.pipe(dest(cfg.outputDir+ '/fonts'));
+// }
+
 async function fonts() {
 	const ttf2woff2 = (await import('gulp-ttf2woff2')).default;
 
+	// Check: if there is no folder, exit
+	if (!existsSync(cfg.srcDir + '/fonts')) {
+		return;
+	}
 	// Copy the original fonts
-	src('src/fonts/**/*', { encoding: false }).pipe(dest('app/fonts'));
+	await new Promise(resolve => {
+		src(cfg.srcDir + '/fonts/**/*', { encoding: false }).pipe(dest(cfg.outputDir+ '/fonts')).on('end', resolve);
+	});
 
 	// Convert only .ttf to .woff2
-	return src('src/fonts/**/*.ttf', { encoding: false })
-		.pipe(plumber({ errorHandler: function () {} }))
+	return src(cfg.srcDir + '/fonts/**/*.ttf', { encoding: false })
+		.pipe(plumber({ errorHandler: plumberHandler }))
 		.pipe(ttf2woff2())
-		.pipe(dest('app/fonts'));
+		.pipe(dest(cfg.outputDir+ '/fonts'));
 }
 
 function styles() {
-	return src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
-		.pipe(plumber({ errorHandler: function () {} }))
+	return src(cfg.srcDir + '/scss/**/*.{scss,sass}', { sourcemaps: true })
+		.pipe(plumber({ errorHandler: plumberHandler }))
 		.pipe(
 			sass({
 				outputStyle: 'expanded', // expanded/compressed
@@ -65,13 +93,13 @@ function styles() {
 			}),
 		)
 
-		.pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
+		.pipe(dest(cfg.outputDir + '/css', { sourcemaps: '.' }))
 		.pipe(browserSync.stream({ once: true }));
 }
 
 function stylesMin() {
 	return (
-		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: false })
+		src(cfg.srcDir + '/scss/**/*.{scss,sass}', { sourcemaps: false })
 			.pipe(
 				sass({
 					outputStyle: 'compressed', // expanded/compressed
@@ -84,32 +112,38 @@ function stylesMin() {
 				}),
 			)
 			// .pipe(csso())
-			.pipe(dest(cfg.outputDir + 'css'))
+			.pipe(dest(cfg.outputDir + '/css'))
 	);
 }
 
 function scripts() {
-	return src(cfg.srcDir + 'js/**/*.js')
-		.pipe(plumber({ errorHandler: function () {} }))
+	return src(cfg.srcDir + '/js/**/*.js')
+		.pipe(plumber({ errorHandler: plumberHandler }))
 		.pipe(concat('script.min.js'))
 		.pipe(terser())
-		.pipe(dest(cfg.outputDir + 'js'))
+		.pipe(dest(cfg.outputDir + '/js'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
 function imageSync() {
-	return src('src/imgs/**/*', { encoding: false })
-		.pipe(dest('app/imgs'))
+	if (!existsSync(cfg.srcDir + '/imgs')) {
+		return Promise.resolve();
+	}
+	return src(cfg.srcDir + '/imgs/**/*', { encoding: false })
+    	.pipe(dest(cfg.outputDir + '/imgs'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
 async function imageSyncMin() {
+		if (!existsSync(cfg.srcDir + '/imgs')) {
+		return Promise.resolve();
+	}
 	const imagemin = (await import('gulp-imagemin')).default;
 	const imageminPngquant = (await import('imagemin-pngquant')).default;
 	const imageminMozjpeg = (await import('imagemin-mozjpeg')).default;
 	const imageminSvgo = (await import('imagemin-svgo')).default;
 
-	return src('src/imgs/**/*', { encoding: false })
+	return src(cfg.srcDir +'/imgs/**/*', { encoding: false })
 		.pipe(
 			imagemin([
 				imageminPngquant({ quality: [0.6, 0.8], speed: 1 }),
@@ -117,7 +151,7 @@ async function imageSyncMin() {
 				imageminSvgo(),
 			]),
 		)
-		.pipe(dest('app/imgs'))
+		.pipe(dest(cfg.outputDir + '/imgs'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
@@ -130,10 +164,10 @@ function browsersync() {
 }
 
 function watching() {
-	watch([cfg.srcDir + 'scss/**/*.scss'], styles);
-	watch([cfg.srcDir + 'js/**/*.js'], scripts);
+	watch([cfg.srcDir + '/scss/**/*.scss'], styles);
+	watch([cfg.srcDir + '/js/**/*.js'], scripts);
 	watch([cfg.srcDir + '/**/*.html'], html);
-	watch([cfg.srcDir + 'imgs/**/*'], imageSync);
+	watch([cfg.srcDir + '/imgs/**/*'], imageSync);
 }
 
 async function clean() {
@@ -148,7 +182,13 @@ async function loadPrettier() {
 
 async function pretty() {
 	const prettier = await loadPrettier();
-	return src(['src/**/*', '!src/imgs/**/*']).pipe(prettier()).pipe(dest('src'));
+	return src([
+		cfg.srcDir + '/**/*',
+		'!' + cfg.srcDir + '/imgs/**/*',
+		'!' + cfg.srcDir + '/*.html'
+	])
+	.pipe(prettier())
+	.pipe(dest(cfg.srcDir));
 }
 
 exports.build = series(
